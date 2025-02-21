4d4f5b8279582fdf9357092e18650db456b3db40
Rajasree Baruri <rbaruri2002@gmail.com>
2025-02-21 12:51:39 +0530

    Update tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,16 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import AppRoutes from './routes/AppRoutes';
 import Navbar from './components/Navbar';
-import TailwindTest from './components/TailwindTest';
 
 function App() {
   return (
     <AuthProvider>
       <Router>
-        <div className="min-h-screen bg-gray-50">
+        <div className="bg-black">
           <Navbar />
           <main>
             <AppRoutes />
           </main>
-          <TailwindTest />
         </div>
       </Router>
     </AuthProvider>
--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import AppRoutes from './routes/AppRoutes';
 import { AuthProvider } from './context/AuthContext';
 import { ApolloProvider } from '@apollo/client';
 import { client } from './api/apolloClient';
-import './index.css'; // Import Tailwind CSS
+import './index.css'; 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
